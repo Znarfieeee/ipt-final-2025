1ae76620d63ddbf26671165456cf05a135611157
znarfieeee <watsszap23@gmail.com>
2025-05-08 21:48:58 +0800

    feat: add new pages for Department, Employees, Workflows, and Requests; update routing in App component

---
+++ frontend/src/pages/Employees.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+function Employees() {
+    return (
+        <>
+            <h1>Employees</h1>
+        </>
+    )
+}
+
+export default Employees
---
+++ frontend/src/pages/Requests.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+function Requests() {
+    return (
+        <>
+            <h1>Requests</h1>
+        </>
+    )
+}
+
+export default Requests
